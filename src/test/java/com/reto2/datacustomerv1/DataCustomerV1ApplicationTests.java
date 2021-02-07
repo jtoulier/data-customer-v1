ea5a9fbc49a3fd32e269555feecac5195ccfbec5
Joseph Toulier Pighi <jtoulier@gmail.com>
2021-02-06 21:12:01 -0500

    Empezando

---
+++ src/test/java/com/reto2/datacustomerv1/DataCustomerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto2.datacustomerv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCustomerV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
